8b1b70f44e84c87378d1e49c2be0ed6b7c294c9b
Hakim <168078248+HakimcHuu@users.noreply.github.com>
2025-05-14 18:18:29 +0300

    Update axios.jsx
    removal of api key

--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -16,5 +16,3 @@ instance.interceptors.response.use(
 export default instance;
 
 
-// Example API call to fetch Netflix Originals
-// https://api.themoviedb.org/3/discover/tv?api_key=8fe705cf998df51a5fcce0ce095a10a3&with_networks=213
